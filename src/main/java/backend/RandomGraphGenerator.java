fd4d1e67d48b5caa31a39e8251752adfa3eac318
Harry.G <garry.garyra@mail.ru>
2022-05-04 13:52:16 +0300

    First commit.

---
+++ src/main/java/backend/RandomGraphGenerator.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.*;
+
+import java.util.stream.Stream;
+
+public abstract class RandomGraphGenerator {
+
+}
+
---
+++ src/main/java/main/MainClass.java
@@ -0,0 +1,9 @@
+package main;
+
+import backend.RandomGraphGenerator;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
